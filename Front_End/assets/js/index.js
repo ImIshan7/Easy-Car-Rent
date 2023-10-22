b692275e12e28b9c3d368a2eef8f10dd520a343f
ImIshan7 <ishandanu999@gmail.com>
2023-10-22 15:06:30 +0100

    Content Updated "Main Page - Index JS"

--- Front_End/assets/js/index.js
+++ Front_End/assets/js/index.js
@@ -13,5 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
+    /*Sticky header on scroll*/
+
+    const selectHeader = document.querySelector('#header');
+    if (selectHeader) {
+        document.addEventListener('scroll', () => {
+            window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+        });
+    }
 
 });
