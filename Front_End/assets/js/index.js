2abcaed42da5bfc5789ae861e724174f28f22371
ImIshan7 <ishandanu999@gmail.com>
2023-10-22 15:11:30 +0100

    Content Updated "Main Page - Index JS"

--- Front_End/assets/js/index.js
+++ Front_End/assets/js/index.js
@@ -47,5 +47,63 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('scroll', navbarlinksActive);
 
 
+    /*Scroll top button*/
+
+    const scrollTop = document.querySelector('.scroll-top');
+    if (scrollTop) {
+        const togglescrollTop = function() {
+            window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+        }
+        window.addEventListener('load', togglescrollTop);
+        document.addEventListener('scroll', togglescrollTop);
+        scrollTop.addEventListener('click', window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        }));
+    }
+
+
+   /* Initiate glightbox*/
+
+    const glightbox = GLightbox({
+        selector: '.glightbox'
+    });
+
+    /*Initiate Counter*/
+    new PureCounter();
+
+
+    /*Init swiper slider with 3 slides at once in desktop view*/
+
+    new Swiper('.slides-3', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 40
+            },
+
+            1200: {
+                slidesPerView: 3,
+            }
+        }
+    });
+
+
 
 });
