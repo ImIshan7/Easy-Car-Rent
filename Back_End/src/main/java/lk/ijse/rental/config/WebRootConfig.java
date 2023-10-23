3c3af54fc0bed102ed69307db2a4599534a26a56
ImIshan7 <ishandanu999@gmail.com>
2023-10-23 09:43:07 +0100

    Content Updated "Add Web RootConfig "

--- Back_End/src/main/java/lk/ijse/rental/config/WebRootConfig.java
+++ Back_End/src/main/java/lk/ijse/rental/config/WebRootConfig.java
@@ -1,4 +1,19 @@
 package lk.ijse.rental.config;
 
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAConfig.class)
+@ComponentScan(basePackages = "lk.ijse.rental.service")
 public class WebRootConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
