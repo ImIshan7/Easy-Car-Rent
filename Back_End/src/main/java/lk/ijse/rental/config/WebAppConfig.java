47bbe4c271d3f0da631bd4b245f573ef5f676451
ImIshan7 <ishandanu999@gmail.com>
2023-10-23 09:46:03 +0100

    Content Updated "Add Web AppConfig "

--- Back_End/src/main/java/lk/ijse/rental/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/rental/config/WebAppConfig.java
@@ -1,4 +1,28 @@
 package lk.ijse.rental.config;
 
-public class WebAppConfig {
+
+import lk.ijse.rental.advisor.AppWideExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {AppWideExceptionHandler.class}, basePackages = "lk.ijse.rental.controller")
+public class WebAppConfig  implements WebMvcConfigurer {
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(500000000);
+        return multipartResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
 }
