462fe97ccd1a7b2c150f053a770c36c95277024a
ImIshan7 <ishandanu999@gmail.com>
2023-10-23 09:52:40 +0100

    Content Updated "Add Advisor "

--- Back_End/src/main/java/lk/ijse/rental/advisor/AppWideExceptionHandler.java
+++ Back_End/src/main/java/lk/ijse/rental/advisor/AppWideExceptionHandler.java
@@ -1,4 +1,19 @@
 package lk.ijse.rental.advisor;
 
+import lk.ijse.rental.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
 public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseUtil handleMyExceptions(RuntimeException e) {
+        System.out.println(e);
+        return new ResponseUtil("Error", e.getMessage(), null);
+    }
 }
--- Back_End/src/main/java/lk/ijse/rental/util/CurrentUser.java
+++ Back_End/src/main/java/lk/ijse/rental/util/CurrentUser.java
@@ -1,4 +1,8 @@
 package lk.ijse.rental.util;
 
+import lk.ijse.rental.dto.UserDTO;
+
 public class CurrentUser {
+
+    public static UserDTO currentUser;
 }
