34e4089b6bf7e6b7ae97e1dfebec7b57469df12d
ImIshan7 <ishandanu999@gmail.com>
2023-10-23 07:52:58 +0100

    Content Updated "Customer JS"

--- Front_End/controllers/CustomerController.js
+++ Front_End/controllers/CustomerController.js
@@ -28,3 +28,31 @@ $("#btnSaveCustomer").click(function () {
         }
     });
 });
+
+
+/*Customer ID Generator*/
+
+function generateCustomerID() {
+    $("#user_Id").val("C00-001");
+    $.ajax({
+        url: userBaseUrl + "reg_User/reg_UserIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let id = resp.value;
+            console.log("id" + id);
+            let tempId = parseInt(id.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#user_Id").val("C00-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#user_Id").val("C00-0" + tempId);
+            } else {
+                $("#user_Id").val("C00-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
+        }
+    });
+}
