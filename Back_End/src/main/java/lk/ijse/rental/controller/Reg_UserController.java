0942df24c1f93b6164fba1a28bcc3086c8345a3a
ImIshan7 <ishandanu999@gmail.com>
2023-10-23 11:02:26 +0100

    Content Updated "Add Reg_UserController"

--- Back_End/src/main/java/lk/ijse/rental/controller/Reg_UserController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/Reg_UserController.java
@@ -1,4 +1,32 @@
 package lk.ijse.rental.controller;
 
+import lk.ijse.rental.dto.Reg_UserDTO;
+import lk.ijse.rental.dto.UserDTO;
+import lk.ijse.rental.embeded.Name;
+import lk.ijse.rental.service.Reg_UserService;
+import lk.ijse.rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
+
 public class Reg_UserController {
+
+    @Autowired
+    private Reg_UserService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil saveUser(@ModelAttribute Reg_UserDTO regUserDTO, @ModelAttribute UserDTO user, @ModelAttribute Name name) {
+        regUserDTO.setName(name);
+        regUserDTO.setUser(user);
+        System.out.println(regUserDTO);
+        service.saveUser(regUserDTO);
+        return new ResponseUtil("OK", "Successfully Registered.!", null);
+    }
+
+
 }
