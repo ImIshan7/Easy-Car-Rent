3f5beba525025824697fdaaa0fb930b6353eac65
ImIshan7 <ishandanu999@gmail.com>
2023-10-23 11:04:58 +0100

    Content Updated "Add Reg_UserController"

--- Back_End/src/main/java/lk/ijse/rental/controller/Reg_UserController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/Reg_UserController.java
@@ -1,8 +1,10 @@
 package lk.ijse.rental.controller;
 
+import lk.ijse.rental.dto.CustomDTO;
 import lk.ijse.rental.dto.Reg_UserDTO;
 import lk.ijse.rental.dto.UserDTO;
 import lk.ijse.rental.embeded.Name;
+import lk.ijse.rental.entity.Reg_User;
 import lk.ijse.rental.service.Reg_UserService;
 import lk.ijse.rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +47,30 @@ public class Reg_UserController {
     public ResponseUtil deleteUser(@RequestParam String id) {
         service.deleteUser(id);
         return new ResponseUtil("OK", "Successfully Deleted. :" + id, null);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/loadAllUsers")
+    public ResponseUtil getAllUser() {
+        return new ResponseUtil("OK", "Successfully Loaded. :", service.getAllUser());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/reg_UserIdGenerate")
+    public @ResponseBody CustomDTO customerIdGenerate() {
+        return service.userIdGenerate();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/searchCustomer", params = {"cus_Id"})
+    public Reg_User searchDriverId(String cus_Id) {
+        return service.searchUserId(cus_Id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/customerCount")
+    public @ResponseBody CustomDTO getSumCustomer() {
+        return service.getSumUser();
     }
 
 
