d84f02c3cb4827546c227af2958f523fad704565
ImIshan7 <ishandanu999@gmail.com>
2023-10-24 08:27:40 +0100

    Content Updated "DriverRepo"

--- Back_End/src/main/java/lk/ijse/rental/repo/DriverRepo.java
+++ Back_End/src/main/java/lk/ijse/rental/repo/DriverRepo.java
@@ -2,8 +2,13 @@ package lk.ijse.rental.repo;
 
 import lk.ijse.rental.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DriverRepo extends JpaRepository<Driver, String> {
+    @Query(value = "SELECT user_Id FROM Driver ORDER BY user_Id DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
+
+
 
 
 }
