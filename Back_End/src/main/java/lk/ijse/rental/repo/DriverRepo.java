77e67909b20ec7245989a97348dd3caea8efacd7
ImIshan7 <ishandanu999@gmail.com>
2023-10-24 08:28:19 +0100

    Content Updated "DriverRepo"

--- Back_End/src/main/java/lk/ijse/rental/repo/DriverRepo.java
+++ Back_End/src/main/java/lk/ijse/rental/repo/DriverRepo.java
@@ -8,6 +8,11 @@ public interface DriverRepo extends JpaRepository<Driver, String> {
     @Query(value = "SELECT user_Id FROM Driver ORDER BY user_Id DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
 
+    @Query(value = "SELECT COUNT(user_Id) FROM Driver", nativeQuery = true)
+    int getSumDriver();
+
+    @Query(value = "SELECT COUNT(user_Id) FROM Driver WHERE driverAvailability='AVAILABLE'", nativeQuery = true)
+    int getSumAvailableDriver();
 
 
 
