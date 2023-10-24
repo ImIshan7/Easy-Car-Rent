f6c6775df85b1bd1afb7cd99abd0920b267c4f17
ImIshan7 <ishandanu999@gmail.com>
2023-10-24 08:28:51 +0100

    Content Updated "DriverRepo"

--- Back_End/src/main/java/lk/ijse/rental/repo/DriverRepo.java
+++ Back_End/src/main/java/lk/ijse/rental/repo/DriverRepo.java
@@ -4,6 +4,8 @@ import lk.ijse.rental.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface DriverRepo extends JpaRepository<Driver, String> {
     @Query(value = "SELECT user_Id FROM Driver ORDER BY user_Id DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
@@ -15,5 +17,10 @@ public interface DriverRepo extends JpaRepository<Driver, String> {
     int getSumAvailableDriver();
 
 
+    @Query(value = "SELECT COUNT(user_Id) FROM Driver WHERE driverAvailability='UNAVAILABLE'", nativeQuery = true)
+    int getSumUnavailableDriver();
+
+    @Query(value = "SELECT * FROM Driver WHERE driverAvailability='AVAILABLE'", nativeQuery = true)
+    List<Driver> availableDrivers();
 
 }
