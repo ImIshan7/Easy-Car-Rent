fc50962621c6effdeac0e5993c8dce69405babd3
ImIshan7 <ishandanu999@gmail.com>
2023-10-24 08:15:52 +0100

    Content Updated "Implement Method DriverServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/DriverServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/DriverServiceImpl.java
@@ -1,4 +1,60 @@
 package lk.ijse.rental.service.impl;
 
-public class DriverServiceImpl {
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.dto.DriverDTO;
+import lk.ijse.rental.entity.Driver;
+import lk.ijse.rental.service.DriverService;
+
+import java.util.ArrayList;
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void saveDriver(DriverDTO dto) {
+
+    }
+
+    @Override
+    public void updateDriver(DriverDTO dto) {
+
+    }
+
+    @Override
+    public void deleteDriver(String reg_ID) {
+
+    }
+
+    @Override
+    public ArrayList<DriverDTO> getAllDriver() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<DriverDTO> getAllAvalabileDriver() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO userIdGenerate() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumAvailableDriver() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumUnavailableDriver() {
+        return null;
+    }
+
+    @Override
+    public Driver searchDriverId(String id) {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumDriver() {
+        return null;
+    }
 }
