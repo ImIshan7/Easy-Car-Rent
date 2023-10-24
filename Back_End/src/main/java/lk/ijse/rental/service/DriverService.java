d94494c651d9eb42a3520e2837d5d5790f3ead30
ImIshan7 <ishandanu999@gmail.com>
2023-10-24 08:14:39 +0100

    Content Updated "Add Driver Service"

--- Back_End/src/main/java/lk/ijse/rental/service/DriverService.java
+++ Back_End/src/main/java/lk/ijse/rental/service/DriverService.java
@@ -1,4 +1,21 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.dto.DriverDTO;
+import lk.ijse.rental.entity.Driver;
+
+import java.util.ArrayList;
+
 public interface DriverService {
+
+    void saveDriver(DriverDTO dto);
+    void updateDriver(DriverDTO dto);
+    void deleteDriver(String reg_ID);
+    ArrayList<DriverDTO> getAllDriver();
+    ArrayList<DriverDTO> getAllAvalabileDriver();
+    CustomDTO userIdGenerate();
+    CustomDTO getSumAvailableDriver();
+    CustomDTO getSumUnavailableDriver();
+    Driver searchDriverId(String id);
+    CustomDTO getSumDriver();
 }
