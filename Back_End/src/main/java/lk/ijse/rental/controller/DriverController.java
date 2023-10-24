43cc8e19d053679d70d66b2abacdfd7ed4f70404
ImIshan7 <ishandanu999@gmail.com>
2023-10-24 08:30:50 +0100

    Content Updated "DriverController"

--- Back_End/src/main/java/lk/ijse/rental/controller/DriverController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/DriverController.java
@@ -1,4 +1,28 @@
 package lk.ijse.rental.controller;
 
+import lk.ijse.rental.dto.DriverDTO;
+import lk.ijse.rental.dto.UserDTO;
+import lk.ijse.rental.embeded.Name;
+import lk.ijse.rental.service.DriverService;
+import lk.ijse.rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/driver")
 public class DriverController {
+
+    @Autowired
+    private DriverService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil saveDriver(@ModelAttribute DriverDTO driverDTO, @ModelAttribute UserDTO userDTO, @ModelAttribute Name name) {
+        driverDTO.setUser(userDTO);
+        driverDTO.setName(name);
+        service.saveDriver(driverDTO);
+        return new ResponseUtil("OK", "Successfully Registered.!", null);
+    }
 }
