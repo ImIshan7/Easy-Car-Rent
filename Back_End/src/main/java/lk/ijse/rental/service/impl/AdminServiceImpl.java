78cd698d6bc1c0d88a6d0ec8fa593c861ff621c6
ImIshan7 <ishandanu999@gmail.com>
2023-10-25 16:42:17 +0100

    Content Updated "AdminServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/AdminServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/AdminServiceImpl.java
@@ -34,7 +34,10 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void updateAdmin(AdminDTO dto) {
-
+        if (!repo.existsById(dto.getUser_Id())) {
+            throw new RuntimeException("Admin Not Exist. Please enter Valid id..!");
+        }
+        repo.save(mapper.map(dto, Admin.class));
     }
 
     @Override
