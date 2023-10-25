eae51235e191c97979b8314ec2607cd051c7092d
ImIshan7 <ishandanu999@gmail.com>
2023-10-25 15:38:01 +0100

    Content Updated "Implement Method CarServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/CarService.java
+++ Back_End/src/main/java/lk/ijse/rental/service/CarService.java
@@ -1,4 +1,23 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.CarDTO;
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.entity.Car;
+
+import java.util.ArrayList;
+
 public interface CarService {
+    void saveCar(CarDTO dto);
+    void updateCar(CarDTO dto);
+    void deleteCar(String car_Id);
+    ArrayList<CarDTO> getAllCar();
+    CustomDTO carIdGenerate();
+    Car searchCarId(String id);
+    CustomDTO getSumCar();
+    CustomDTO getSumAvailableCar();
+    CustomDTO getSumReservedCar();
+    CustomDTO getSumMaintainCar();
+    CustomDTO getSumUnderMaintainCar();
+    ArrayList<CarDTO> getFilerData(String type,String fuelType);
+    ArrayList<CarDTO> filterCarDetails(String name, String fuel_Type,String type, String transmission_Type);
 }
--- Back_End/src/main/java/lk/ijse/rental/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/CarServiceImpl.java
@@ -1,4 +1,80 @@
 package lk.ijse.rental.service.impl;
 
-public class CarServiceImpl {
+import lk.ijse.rental.dto.CarDTO;
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.entity.Car;
+import lk.ijse.rental.service.CarService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+
+@Service
+@Transactional
+public class CarServiceImpl  implements CarService {
+    @Override
+    public void saveCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public void updateCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCar(String car_Id) {
+
+    }
+
+    @Override
+    public ArrayList<CarDTO> getAllCar() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO carIdGenerate() {
+        return null;
+    }
+
+    @Override
+    public Car searchCarId(String id) {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumCar() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumAvailableCar() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumReservedCar() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumMaintainCar() {
+        return null;
+    }
+
+    @Override
+    public CustomDTO getSumUnderMaintainCar() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<CarDTO> getFilerData(String type, String fuelType) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<CarDTO> filterCarDetails(String name, String fuel_Type, String type, String transmission_Type) {
+        return null;
+    }
 }
