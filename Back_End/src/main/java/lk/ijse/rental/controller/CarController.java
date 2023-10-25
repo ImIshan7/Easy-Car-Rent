bbcfc19cc459dd28031c3a8be20aa7a59bf0fdf1
ImIshan7 <ishandanu999@gmail.com>
2023-10-25 16:04:25 +0100

    Content Updated "Car Controller"

--- Back_End/src/main/java/lk/ijse/rental/controller/CarController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/CarController.java
@@ -1,4 +1,36 @@
 package lk.ijse.rental.controller;
 
+import lk.ijse.rental.dto.CarDTO;
+import lk.ijse.rental.embeded.ImageDTO;
+import lk.ijse.rental.embeded.Rate;
+import lk.ijse.rental.service.CarService;
+import lk.ijse.rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/car")
 public class CarController {
+    @Autowired
+    private CarService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseUtil saveCar(@ModelAttribute CarDTO dto, @ModelAttribute Rate rate, @ModelAttribute ImageDTO image) {
+        dto.setImage(image);
+        dto.setRent_Duration_Price(rate);
+
+        System.out.println(dto);
+        service.saveCar(dto);
+        return new ResponseUtil("OK", "Successfully Registered.!", null);
+    }
+
+
+
+
+
+
+
 }
