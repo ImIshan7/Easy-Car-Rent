9059b722458b1dc4c9d7632e9b4a0e0eb85fa9b6
ImIshan7 <ishandanu999@gmail.com>
2023-10-25 15:47:24 +0100

    Content Updated "CarServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/repo/CarRepo.java
+++ Back_End/src/main/java/lk/ijse/rental/repo/CarRepo.java
@@ -1,4 +1,16 @@
 package lk.ijse.rental.repo;
 
-public interface CarRepo {
+import lk.ijse.rental.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+public interface CarRepo extends JpaRepository<Car, String> {
+    @Query(value = "SELECT car_Id FROM Car ORDER BY car_Id DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
+
+    @Query(value = "SELECT COUNT(car_Id) FROM Car", nativeQuery = true)
+    int getSumCar();
+
+
 }
--- Back_End/src/main/java/lk/ijse/rental/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/CarServiceImpl.java
@@ -2,19 +2,62 @@ package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.CarDTO;
 import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.embeded.Image;
 import lk.ijse.rental.entity.Car;
+import lk.ijse.rental.repo.CarRepo;
 import lk.ijse.rental.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 
 @Service
 @Transactional
 public class CarServiceImpl  implements CarService {
+
+    @Autowired
+    private CarRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+
+
     @Override
     public void saveCar(CarDTO dto) {
+
+        Car car = new Car(dto.getCar_Id(), dto.getName(), dto.getBrand(), dto.getType(), new Image(), dto.getNumber_Of_Passengers(), dto.getTransmission_Type(), dto.getFuel_Type(), dto.getRent_Duration_Price(), dto.getPrice_Extra_KM(), dto.getRegistration_Number(), dto.getFree_Mileage(), dto.getColor(), dto.getVehicleAvailabilityType());
+        if (repo.existsById(dto.getCar_Id())) {
+            throw new RuntimeException("Car Already Exist. Please enter another id..!");
+        }
+
+        try {
+
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            File uploadsDir = new File(projectPath + "/uploads");
+            System.out.println(projectPath);
+            uploadsDir.mkdir();
+
+            dto.getImage().getFront_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getFront_View().getOriginalFilename()));
+            dto.getImage().getBack_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getBack_View().getOriginalFilename()));
+            dto.getImage().getSide_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getSide_View().getOriginalFilename()));
+            dto.getImage().getInterior().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getInterior().getOriginalFilename()));
+
+            car.getImage().setFront_View("uploads/"+dto.getImage().getFront_View().getOriginalFilename());
+            car.getImage().setBack_View("uploads/"+dto.getImage().getBack_View().getOriginalFilename());
+            car.getImage().setSide_View("uploads/"+dto.getImage().getSide_View().getOriginalFilename());
+            car.getImage().setInterior("uploads/"+dto.getImage().getInterior().getOriginalFilename());
+
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(car);
+        repo.save(car);
 
     }
 
