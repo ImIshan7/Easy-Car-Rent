4b951dfcaf2cc86dfeb7eef6875ff79c6d6c5731
ImIshan7 <ishandanu999@gmail.com>
2023-10-26 19:11:45 +0100

    Content Updated "DashBoard Controller JS"

---
+++ Front_End/controllers/DashBoardController.js
@@ -0,0 +1,21 @@
+let dashboard = "http://localhost:8087/Back_End_war/";
+
+
+/*Car Count*/
+
+$("#carCount").val("00");
+$.ajax({
+    url: dashboard + "car/carCount",
+    method: "GET",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (resp) {
+        let num = resp.count;
+        $("#carCount").text(num);
+
+    },
+    error: function (ob, statusText, error) {
+
+    }
+});
+
