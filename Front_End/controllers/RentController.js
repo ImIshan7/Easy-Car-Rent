092194668427f1fa677585c75bfbbb58bfc4528c
ImIshan7 <ishandanu999@gmail.com>
2023-10-26 17:20:22 +0100

    Content Updated "Rent Controller JS"

--- Front_End/controllers/RentController.js
+++ Front_End/controllers/RentController.js
@@ -25,3 +25,114 @@ function loadAllRentDetails() {
         }
     });
 }
+
+
+/*Search Rents*/
+
+$("#search_Id").on("keypress", function (event) {
+    if (event.which === 13) {
+        var search = $("#search_Id").val();
+        $("#retManage").empty();
+        $.ajax({
+            url: RentAllManageBaseUrl + "rent/searchDtails?search_Id=" + search,
+            method: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (i) {
+                console.log(i);
+                console.log(i.rentID)
+                let row = "<tr><td>" + i.rentID + "</td><td>" + i.rentDetails.at().carID + "</td><td>" + i.regUser.user_Id + "</td><td>" + i.rentDetails.at().driverID + "</td><td>" + i.requestType + "</td><td>" + i.rentType + "</td><td>" + i.pickUpDate + "</td><td>" + i.pickUpTime + "</td><td>" + i.returnTime + "</td><td>" + i.returnDate + "</td><td>" + i.location + "</td></tr>";
+                $("#retManage").append(row);
+                blindClickEventsRent();
+            },
+            error: function (error) {
+                loadAllRentDetails();
+                let message = JSON.parse(error.responseText).message;
+                emptyMassage(message);
+            }
+        })
+    }
+
+});
+let carID;
+
+function blindClickEventsRent() {
+    $("#retManage>tr").on("click", function () {
+        let requestRentId = $(this).children().eq(0).text();
+        let driverId = $(this).children().eq(3).text();
+        let userID = $(this).children().eq(2).text();
+        let requestState = $(this).children().eq(5).text();
+        carID = $(this).children().eq(1).text();
+
+
+        $("#requestRentId").val(requestRentId);
+        $("#driverId").val(driverId);
+        $("#userID").val(userID);
+        $("#requestState").val(requestState);
+
+        $("#rentID").val(requestRentId);
+        $("#driverId").append(`<option>${driverId}</option>`);
+        loadAllCars();
+    });
+}
+
+$.ajax({
+    url: RentAllManageBaseUrl + "driver/loadAvalabilityDrivers",
+    method: "GET",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (res) {
+        console.log(res.data);
+
+        for (let i of res.data) {
+            let driverId = i.user_Id;
+
+            $("#driverId").append(`<option>${driverId}</option>`);
+        }
+    },
+    error: function (error) {
+        let message = JSON.parse(error.responseText).message;
+        emptyMassage(message);
+    }
+});
+
+$("#btnAccept").on("click", function () {
+    let rentID = $("#requestRentId").val();
+    let driverID = $("#driverId").val();
+    $.ajax({
+        url: RentAllManageBaseUrl + "rent/rentConfrom/?rentID=" + rentID + "&driverId=" + driverID,
+        method: "post",
+        dataType: "json",
+        success: function (res) {
+            saveUpdateAlert("Booking Conform", res.message);
+            $("#retManage").empty();
+            loadAllRentDetails();
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            emptyMassage(message);
+        }
+    });
+
+});
+
+$("#btnReject").on("click", function () {
+    let rentID = $("#requestRentId").val();
+    let driverID = $("#driverId").val();
+    $.ajax({
+        url: RentAllManageBaseUrl + "rent/rentReject/?rentID=" + rentID + "&driverId=" + driverID,
+        method: "post",
+        dataType: "json",
+        success: function (res) {
+            saveUpdateAlert("Booking Reject", res.message);
+            $("#retManage").empty();
+            loadAllRentDetails();
+        },
+        error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            emptyMassage(message);
+        }
+    });
+
+});
+
