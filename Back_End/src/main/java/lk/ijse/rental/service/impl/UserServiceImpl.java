0f2885b806ee257c12366e704cf01830b3c81abc
ImIshan7 <ishandanu999@gmail.com>
2023-10-27 18:24:34 +0100

    Content Updated "UserServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/UserServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.UserDTO;
+import lk.ijse.rental.repo.UserRepo;
 import lk.ijse.rental.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +14,20 @@ import java.util.ArrayList;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
+
     @Override
     public ArrayList<UserDTO> getAllRegUsers() {
-        return null;
+
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<UserDTO>>() {
+        }.getType());
     }
 
     @Override
