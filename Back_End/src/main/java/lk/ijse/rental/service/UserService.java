d8e6dcd3e3fd26934e4da337acf67e8527d07de4
ImIshan7 <ishandanu999@gmail.com>
2023-10-27 18:21:45 +0100

    Content Updated "User Service"

--- Back_End/src/main/java/lk/ijse/rental/service/UserService.java
+++ Back_End/src/main/java/lk/ijse/rental/service/UserService.java
@@ -1,4 +1,11 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.UserDTO;
+
+import java.util.ArrayList;
+
 public interface UserService {
+    ArrayList<UserDTO> getAllRegUsers();
+
+    UserDTO getRegUsers(String username,String password);
 }
