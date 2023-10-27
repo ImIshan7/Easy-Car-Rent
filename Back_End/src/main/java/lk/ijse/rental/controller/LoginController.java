80b3171fddf668a2104bc5b3617980bc13ccf652
ImIshan7 <ishandanu999@gmail.com>
2023-10-27 18:26:33 +0100

    Content Updated "UserController"

--- Back_End/src/main/java/lk/ijse/rental/controller/LoginController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/LoginController.java
@@ -2,12 +2,26 @@ package lk.ijse.rental.controller;
 
 
 
+import lk.ijse.rental.service.UserService;
+import lk.ijse.rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/loginForm")
 public class LoginController {
+
+    @Autowired
+    private UserService service;
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllRegUser(){
+        System.out.println(service.getAllRegUsers());
+        return new ResponseUtil("OK","Successfully Loaded..!",service.getAllRegUsers());
+    }
 
 
 
