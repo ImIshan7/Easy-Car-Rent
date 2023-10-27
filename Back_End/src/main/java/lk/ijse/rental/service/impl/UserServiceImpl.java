515353acaeaf8dc1d1a4ffcf9120d622069db72b
ImIshan7 <ishandanu999@gmail.com>
2023-10-27 18:23:05 +0100

    Content Updated "Implement In Method UserServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/UserServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/UserServiceImpl.java
@@ -1,4 +1,22 @@
 package lk.ijse.rental.service.impl;
 
-public class UserServiceImpl {
+import lk.ijse.rental.dto.UserDTO;
+import lk.ijse.rental.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Override
+    public ArrayList<UserDTO> getAllRegUsers() {
+        return null;
+    }
+
+    @Override
+    public UserDTO getRegUsers(String username, String password) {
+        return null;
+    }
 }
