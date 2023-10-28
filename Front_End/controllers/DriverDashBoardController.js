712ec3cba43c2f3c9552cae5d331dfe499ab7031
ImIshan7 <ishandanu999@gmail.com>
2023-10-28 16:59:18 +0100

    Content Updated "Driver DashBoard JS"

---
+++ Front_End/controllers/DriverDashBoardController.js
@@ -0,0 +1,14 @@
+let userbasurl = "http://localhost:8087/Back_End_war/";
+
+let user;
+
+$.ajax({
+    url: userbasurl + "loginForm/current",
+    method: "get",
+    success: function (res) {
+        user = res.data.user_Id;
+        console.log(res.data)
+        $("#search_Id").val(res.data.user_Id);
+    }
+});
+
