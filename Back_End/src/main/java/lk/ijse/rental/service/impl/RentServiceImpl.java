dfb0fa03140db4ced1f6eef94f814f9d1e4769c3
ImIshan7 <ishandanu999@gmail.com>
2023-10-31 14:29:55 +0000

    Content Updated " RentService Impl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/RentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/RentServiceImpl.java
@@ -2,6 +2,11 @@ package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.CustomDTO;
 import lk.ijse.rental.dto.RentDTO;
+import lk.ijse.rental.entity.Car;
+import lk.ijse.rental.entity.Driver;
+import lk.ijse.rental.entity.Rent;
+import lk.ijse.rental.entity.RentDetails;
+import lk.ijse.rental.enums.RequestType;
 import lk.ijse.rental.repo.CarRepo;
 import lk.ijse.rental.repo.DriverRepo;
 import lk.ijse.rental.repo.RentRepo;
@@ -12,6 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static lk.ijse.rental.enums.AvailabilityType.UNAVAILABLE;
 
 
 @Service
@@ -34,6 +43,33 @@ public class RentServiceImpl implements RentService {
 
     @Override
     public void bookingCars(RentDTO dto) {
+        Rent rent = mapper.map(dto, Rent.class);
+        if (rentRepo.existsById(dto.getRentID())) {
+            throw new RuntimeException("Booking" + dto.getRentID() + " Already added.!");
+        }
+
+        if (dto.getRequestType().equals(RequestType.YES)) {
+            List<Driver> drivers = driverRepo.availableDrivers();
+            int x;
+
+            for (RentDetails rentDetails : rent.getRentDetails()) {
+                x = new Random().nextInt(drivers.size());
+                rentDetails.setDriverID(drivers.get(x).getUser_Id());
+                Car car = carRepo.findById(rentDetails.getCarID()).get();
+                car.setVehicleAvailabilityType(UNAVAILABLE);
+                carRepo.save(car);
+                drivers.get(x).setDriverAvailability(UNAVAILABLE);
+                driverRepo.save(drivers.get(x));
+            }
+        } else if (dto.getRequestType().equals(RequestType.NO)) {
+            for (RentDetails rentDetails : rent.getRentDetails()) {
+                Car car = carRepo.findById(rentDetails.getCarID()).get();
+                car.setVehicleAvailabilityType(UNAVAILABLE);
+                carRepo.save(car);
+            }
+        }
+
+        rentRepo.save(rent);
 
     }
 
