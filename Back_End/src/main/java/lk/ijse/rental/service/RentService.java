a27c5c47b2931ad6ee42f506c2c71e5a7088edec
ImIshan7 <ishandanu999@gmail.com>
2023-10-31 14:19:09 +0000

    Content Updated "Rent Service"

--- Back_End/src/main/java/lk/ijse/rental/service/RentService.java
+++ Back_End/src/main/java/lk/ijse/rental/service/RentService.java
@@ -1,4 +1,14 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.dto.RentDTO;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface RentService {
+
+    CustomDTO rentIdGenerate();
+    void bookingCars(@RequestBody RentDTO dto);
+    CustomDTO getSumOfBooking();
+
+
 }
