33b2080a48ea43a98aa3a37e2f8c1403505d2004
ImIshan7 <ishandanu999@gmail.com>
2023-10-31 15:00:32 +0000

    Content Updated " RentService Controller"

--- Back_End/src/main/java/lk/ijse/rental/controller/RentController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/RentController.java
@@ -1,4 +1,18 @@
 package lk.ijse.rental.controller;
 
+
+import lk.ijse.rental.service.RentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/rent")
 public class RentController {
+    @Autowired
+    private RentService service;
+
+
 }
