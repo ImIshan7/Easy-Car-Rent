8a1d6f2077183b06c25a7244a2ed725d226fead1
ImIshan7 <ishandanu999@gmail.com>
2023-11-01 09:53:34 +0000

    Content Updated "Payment DTO"

--- Back_End/src/main/java/lk/ijse/rental/dto/PaymentDTO.java
+++ Back_End/src/main/java/lk/ijse/rental/dto/PaymentDTO.java
@@ -1,4 +1,28 @@
 package lk.ijse.rental.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
 public class PaymentDTO {
+
+    private String paymentId;
+    private String rentId;
+    private String paymentType;
+    private LocalDate date;
+    private LocalTime time;
+    private Double lostDamage;
+    private Double rentFee;
+    private Double driverFee;
+    private Double total;
+
 }
