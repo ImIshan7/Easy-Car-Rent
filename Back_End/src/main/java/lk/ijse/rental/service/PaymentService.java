792c70aae1a64335f2d4943074f77e8a02ccb092
ImIshan7 <ishandanu999@gmail.com>
2023-11-01 09:55:45 +0000

    Content Updated "Payment Service"

--- Back_End/src/main/java/lk/ijse/rental/service/PaymentService.java
+++ Back_End/src/main/java/lk/ijse/rental/service/PaymentService.java
@@ -1,4 +1,13 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.dto.PaymentDTO;
+
+import java.util.ArrayList;
+
 public interface PaymentService {
+
+    CustomDTO paymentIdGenerate();
+    void savePayment(PaymentDTO dto,String rentID);
+    ArrayList<PaymentDTO> getAllPayment();
 }
