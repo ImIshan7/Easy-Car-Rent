30b664ae2d99ee6dd8c6067906b9ee9330e2a9b5
ImIshan7 <ishandanu999@gmail.com>
2023-11-01 10:10:09 +0000

    Content Updated "PaymentController"

--- Back_End/src/main/java/lk/ijse/rental/controller/PaymentController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/PaymentController.java
@@ -1,4 +1,27 @@
 package lk.ijse.rental.controller;
 
+
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/payment")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/paymentIdGenerate")
+    public @ResponseBody CustomDTO customerIdGenerate() {
+        return service.paymentIdGenerate();
+    }
+
+
+
+
 }
