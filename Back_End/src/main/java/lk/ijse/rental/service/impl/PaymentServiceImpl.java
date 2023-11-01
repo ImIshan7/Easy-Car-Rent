665cbc5a0eb352d2474cf6cf3b2f8bf7ab75f898
ImIshan7 <ishandanu999@gmail.com>
2023-11-01 10:02:11 +0000

    Content Updated "PaymentServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/PaymentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/PaymentServiceImpl.java
@@ -2,6 +2,10 @@ package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.CustomDTO;
 import lk.ijse.rental.dto.PaymentDTO;
+import lk.ijse.rental.entity.Car;
+import lk.ijse.rental.entity.Driver;
+import lk.ijse.rental.entity.Payment;
+import lk.ijse.rental.entity.Rent;
 import lk.ijse.rental.repo.CarRepo;
 import lk.ijse.rental.repo.DriverRepo;
 import lk.ijse.rental.repo.PaymentRepo;
@@ -13,6 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+
+import static lk.ijse.rental.enums.AvailabilityType.AVAILABLE;
+import static lk.ijse.rental.enums.AvailabilityType.UNDER_MAINTAIN;
+import static lk.ijse.rental.enums.RentRequest.PAY;
 
 
 @Service
@@ -44,6 +52,31 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public void savePayment(PaymentDTO dto, String rentID) {
+
+        Payment payment = mapper.map(dto, Payment.class);
+        Rent rent = rentRepo.findById(rentID).get();
+        if (rent.getRentDetails().get(0).getDriverID() != null) {
+
+            Driver drivers = driverRepo.findById(rent.getRentDetails().get(0).getDriverID()).get();
+            drivers.setDriverAvailability(AVAILABLE);
+            driverRepo.save(drivers);
+
+            Car car = carRepo.findById(rent.getRentDetails().get(0).getCarID()).get();
+            car.setVehicleAvailabilityType(UNDER_MAINTAIN);
+            carRepo.save(car);
+
+            rent.setRentType(PAY);
+            rentRepo.save(rent);
+        }
+        if (rent.getRentDetails().get(0).getDriverID() == null) {
+            Car car = carRepo.findById(rent.getRentDetails().get(0).getCarID()).get();
+            car.setVehicleAvailabilityType(UNDER_MAINTAIN);
+            carRepo.save(car);
+
+            rent.setRentType(PAY);
+            rentRepo.save(rent);
+        }
+        paymentRepo.save(payment);
 
     }
 
