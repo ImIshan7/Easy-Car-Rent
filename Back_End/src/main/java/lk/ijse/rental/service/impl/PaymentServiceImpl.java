e1ba3463935f70d09598d153637a95c4ed5cfbb7
ImIshan7 <ishandanu999@gmail.com>
2023-11-01 09:57:18 +0000

    Content Updated "method implement in PaymentServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/PaymentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/PaymentServiceImpl.java
@@ -1,4 +1,29 @@
 package lk.ijse.rental.service.impl;
 
-public class PaymentServiceImpl {
+import lk.ijse.rental.dto.CustomDTO;
+import lk.ijse.rental.dto.PaymentDTO;
+import lk.ijse.rental.service.PaymentService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+
+@Service
+@Transactional
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public CustomDTO paymentIdGenerate() {
+        return null;
+    }
+
+    @Override
+    public void savePayment(PaymentDTO dto, String rentID) {
+
+    }
+
+    @Override
+    public ArrayList<PaymentDTO> getAllPayment() {
+        return null;
+    }
 }
