816a8de8071084118f384b1bdaaebdc08329faee
ImIshan7 <ishandanu999@gmail.com>
2023-11-02 09:54:30 +0000

    Content Updated "IncomeServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/repo/IncomeRepo.java
+++ Back_End/src/main/java/lk/ijse/rental/repo/IncomeRepo.java
@@ -1,4 +1,12 @@
 package lk.ijse.rental.repo;
 
-public interface IncomeRepo {
+import lk.ijse.rental.entity.Rent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
+
+public interface IncomeRepo extends JpaRepository<Rent, String> {
+    @Query(value = "SELECT payment.date,count(payment.paymentID),sum(payment.total) FROM Rent INNER JOIN Payment ON Rent.rentID = Payment.rentID GROUP BY date", nativeQuery = true)
+    ArrayList dailyIncome();
 }
--- Back_End/src/main/java/lk/ijse/rental/service/impl/IncomeServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/IncomeServiceImpl.java
@@ -1,7 +1,10 @@
 package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.IncomeDTO;
+import lk.ijse.rental.repo.IncomeRepo;
 import lk.ijse.rental.service.IncomeService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +13,18 @@ import java.util.ArrayList;
 @Service
 @Transactional
 public class IncomeServiceImpl implements IncomeService {
+
+    @Autowired
+    private IncomeRepo incomeService;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
+
     @Override
     public ArrayList<IncomeDTO> dailyIncome() {
-        return null;
+        return new ArrayList<IncomeDTO>(incomeService.dailyIncome());
     }
 
     @Override
