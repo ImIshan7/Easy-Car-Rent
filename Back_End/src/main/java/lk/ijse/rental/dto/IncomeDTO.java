40fbaf1f5321a6aef95801287e8131152f9a2eb7
ImIshan7 <ishandanu999@gmail.com>
2023-11-02 09:44:58 +0000

    Content Updated "Income DTO"

--- Back_End/src/main/java/lk/ijse/rental/dto/IncomeDTO.java
+++ Back_End/src/main/java/lk/ijse/rental/dto/IncomeDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.rental.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class IncomeDTO {
+    private String date;
+    private int count;
+    private double total;
 }
