ab0ed50c9f6b9fccd159eff8ec96eff99b316459
ImIshan7 <ishandanu999@gmail.com>
2023-11-02 09:48:18 +0000

    Content Updated "Implement IN Method In IncomeServiceImpl"

--- Back_End/src/main/java/lk/ijse/rental/service/impl/IncomeServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/rental/service/impl/IncomeServiceImpl.java
@@ -1,4 +1,27 @@
 package lk.ijse.rental.service.impl;
 
-public class IncomeServiceImpl {
+import lk.ijse.rental.dto.IncomeDTO;
+import lk.ijse.rental.service.IncomeService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class IncomeServiceImpl implements IncomeService {
+    @Override
+    public ArrayList<IncomeDTO> dailyIncome() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<IncomeDTO> monthlyIncome() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<IncomeDTO> AnnuallyIncome() {
+        return null;
+    }
 }
