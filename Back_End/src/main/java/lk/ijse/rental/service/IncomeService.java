f834f8e47e0914eb0bfb075bf2459c578d1f36eb
ImIshan7 <ishandanu999@gmail.com>
2023-11-02 09:46:23 +0000

    Content Updated "Income Service"

--- Back_End/src/main/java/lk/ijse/rental/service/IncomeService.java
+++ Back_End/src/main/java/lk/ijse/rental/service/IncomeService.java
@@ -1,4 +1,12 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.IncomeDTO;
+
+import java.util.ArrayList;
+
 public interface IncomeService {
+
+    ArrayList<IncomeDTO> dailyIncome();
+    ArrayList<IncomeDTO> monthlyIncome();
+    ArrayList<IncomeDTO> AnnuallyIncome();
 }
