f0f4efcd3fe7a226963b8d7db3fcc9d9ed676afa
ImIshan7 <ishandanu999@gmail.com>
2023-11-02 10:03:31 +0000

    Content Updated "IncomeServiceController"

--- Back_End/src/main/java/lk/ijse/rental/controller/IncomeController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/IncomeController.java
@@ -1,11 +1,13 @@
 package lk.ijse.rental.controller;
 
 
+import lk.ijse.rental.dto.IncomeDTO;
 import lk.ijse.rental.service.IncomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -14,6 +16,12 @@ public class IncomeController {
 
     @Autowired
     private IncomeService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/dailyIncome")
+    public @ResponseBody ArrayList<IncomeDTO> dailyIncome() {
+        return service.dailyIncome();
+    }
 
 
 }
