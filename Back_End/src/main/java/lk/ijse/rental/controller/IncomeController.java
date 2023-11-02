c0a7e2830d4c8709440c566fe7761f0f7dfe7724
ImIshan7 <ishandanu999@gmail.com>
2023-11-02 10:03:13 +0000

    Content Updated "IncomeServiceController"

--- Back_End/src/main/java/lk/ijse/rental/controller/IncomeController.java
+++ Back_End/src/main/java/lk/ijse/rental/controller/IncomeController.java
@@ -1,4 +1,19 @@
 package lk.ijse.rental.controller;
 
+
+import lk.ijse.rental.service.IncomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/income")
 public class IncomeController {
+
+    @Autowired
+    private IncomeService service;
+
+
 }
